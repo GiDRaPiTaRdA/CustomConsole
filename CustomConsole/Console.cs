0733383fb7e77db9de307e5d6f086cf24da4e1c6
GiDRaPiTaRdA <xxmaxdrivezz@gmail.com>
2019-11-18 13:51:50 +0200

    source

---
+++ CustomConsole/Console.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomConsole
+{
+    public static class Console
+    {
+        public static void WriteLine() => System.Console.WriteLine();
+        public static void WriteLine(object obj)=> System.Console.WriteLine(obj);
+        public static void WriteLine(object obj, ConsoleColor color) => color.Color(()=>System.Console.WriteLine(obj));
+        public static void Write(object obj) => System.Console.Write(obj);
+        public static void Write(object obj, ConsoleColor color) => color.Color(() => System.Console.Write(obj));
+        public static string ReadLine()
+        {
+            ConsoleColor.Yellow.Color(() =>Write("$ "));
+
+            return System.Console.ReadLine(); ;
+        }
+
+        private static void Color(this ConsoleColor color, Action action)
+        {
+            ConsoleColor temp = System.Console.ForegroundColor;
+
+            System.Console.ForegroundColor = color;
+            action?.Invoke();
+            System.Console.ForegroundColor = temp;
+        }
+
+        public static ConsoleKeyInfo ReadKey(bool intercept = false) => System.Console.ReadKey(intercept);
+    }
+}
---
+++ CustomConsole/ConsoleMenu.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CustomConsole
+{
+    public static class ConsoleMenu
+    {
+        public static bool Menu(Dictionary<string, Action> actions)
+        {
+            string command = Console.ReadLine();
+
+            if (actions.ContainsKey(command))
+            {
+                actions.RunCommand(command);
+            }
+            else
+            {
+                switch (command)
+                {
+                    case "help":
+                        Help(actions);
+                        break;
+                    case "0":
+                        Console.WriteLine("Exit");
+                        return false;
+                    default:
+                        NoSuchCommand(command);
+                        break;
+                }
+            }
+
+            return true;
+        }
+
+        public static void RunCommand(this Dictionary<string, Action> commandsDiscionary, string command)
+        {
+            Console.WriteLine($"->{command}", ConsoleColor.Yellow);
+
+            if (commandsDiscionary.ContainsKey(command))
+            {
+                commandsDiscionary[command].Invoke();
+            }
+            else
+            {
+                NoSuchCommand(command);
+            }          
+        }
+
+        private static void NoSuchCommand(string command)
+        {
+            Console.WriteLine("No such command " + command);
+        }
+
+        public static void Help(Dictionary<string, Action> commandsDiscionary)
+        {
+            commandsDiscionary.Select(a => a.Key).ToList().ForEach(a1 => Console.Write($"[{a1}] "));
+            Console.WriteLine();
+        }
+    }
+}
